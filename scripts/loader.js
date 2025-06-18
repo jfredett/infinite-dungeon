1b609c8c3ac3d3dc368a5b2c2870b370375d6d66
Joe Fredette <jfredett@gmail.com>
2025-06-18 09:07:31 -0400

    vault backup: 2025-06-18 09:07:31

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,17 +2,15 @@ Hooks.on("init", function() {
     console.log("Welcome to the I N F I N I T E   D U N G E O N");
 });
 
-
 Hooks.once("ready", () => {
   game.usageDie = {
     rollUsageDie
   };
 
-  // Optional: Add UI chat command handler
   Hooks.on("chatMessage", (chatLog, message, chatData) => {
     if (!message.startsWith("/ud ")) return;
     const args = message.slice(4).trim();
-    const result = rollUsageDie(args);
+    const result = rollUsageDie(args, false);
     ChatMessage.create({ content: result.message });
     return false;
   });
